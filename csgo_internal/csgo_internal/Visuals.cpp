8e75de180d502524c2255fdd983b6f96b40f23f9
cantbeatme <schoolgaming8@hotmail.com>
2016-08-11 21:15:22 -0400

    Updated~

--- csgo_internal/csgo_internal/Visuals.cpp
+++ csgo_internal/csgo_internal/Visuals.cpp
@@ -65,7 +65,7 @@ void CVisuals::PlayerESP( int index )
 	float height = ( pos.y - top.y );
 	float width = height / 4.f;
 
-	if (strstr(Entity->GetClientClass()->m_pNetworkName, charenc("CHostage")))
+	if( Entity->GetClientClass()->m_ClassID == 83 ) // CHostage ClassID - Thx
 	{
 		D::DrawString(F::ESP, top.x, top.y + 3, Color::Green(), FONT_CENTER, charenc("Hostage"));
 		return;
