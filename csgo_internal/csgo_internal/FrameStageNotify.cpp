9bcf01c3f6376eb67c778ac2124e47baadbb8bb3
bazzz1512 <bazzz1512@users.noreply.github.com>
2017-05-14 22:11:13 +0200

    PVS Fix
    Simple PVS fix by Dr_Pepper

--- csgo_internal/csgo_internal/FrameStageNotify.cpp
+++ csgo_internal/csgo_internal/FrameStageNotify.cpp
@@ -10,20 +10,34 @@ void __stdcall Hooks::FrameStageNotify( ClientFrameStage_t stage )
 	QAngle* aim_punch = nullptr;
 	QAngle* view_punch = nullptr;
 
-	if( I::Engine->IsInGame() && stage == FRAME_RENDER_START && Vars.Visuals.Removals.VisualRecoil )
-	{
-		if( G::LocalPlayer && G::LocalPlayer->GetAlive() )
+	if (I::Engine->IsInGame()) {
+		if (stage == FRAME_RENDER_START && Vars.Visuals.Removals.VisualRecoil)
 		{
-			aim_punch = ( QAngle* )( ( DWORD )G::LocalPlayer + offsets.m_aimPunchAngle );
-			view_punch = ( QAngle* )( ( DWORD )G::LocalPlayer + offsets.m_viewPunchAngle );
+			if (G::LocalPlayer && G::LocalPlayer->GetAlive())
+			{
+				aim_punch = (QAngle*)((DWORD)G::LocalPlayer + offsets.m_aimPunchAngle);
+				view_punch = (QAngle*)((DWORD)G::LocalPlayer + offsets.m_viewPunchAngle);
 
-			aim_punch_old = *aim_punch;
-			view_punch_old = *view_punch;
+				aim_punch_old = *aim_punch;
+				view_punch_old = *view_punch;
 
-			*aim_punch = QAngle( 0, 0, 0 );
-			*view_punch = QAngle( 0, 0, 0 );
+				*aim_punch = QAngle(0, 0, 0);
+				*view_punch = QAngle(0, 0, 0);
+			}
 		}
-	}
+		if (stage == FRAME_RENDER_START)
+		{
+			for (int i = 1; i <= I::Globals->maxClients; i++)
+			{
+				if (i == I::Engine->GetLocalPlayer()) continue;
+
+				CBaseEntity* pCurEntity = I::ClientEntList->GetClientEntity(i);
+				if (!pCurEntity) continue;
+
+				*(int*)((uintptr_t)pCurEntity + 0xA30) = I::Globals ->framecount; //we'll skip occlusion checks now
+				*(int*)((uintptr_t)pCurEntity + 0xA28) = 0;//clear occlusion flags
+			}
+		}
 
 	oFrameStageNotify( stage );
 
