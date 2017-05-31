433b90df25b9198cf9f1fecc1af19bd598baa35b
A5- <a5@0wn3d.me>
2017-05-31 17:20:25 +0100

    Clean up

--- csgo_internal/csgo_internal/FrameStageNotify.cpp
+++ csgo_internal/csgo_internal/FrameStageNotify.cpp
@@ -2,50 +2,55 @@
 
 FrameStageNotifyFn oFrameStageNotify;
 
-void __stdcall Hooks::FrameStageNotify( ClientFrameStage_t stage )
+void __stdcall Hooks::FrameStageNotify(ClientFrameStage_t stage)
 {
-	QAngle aim_punch_old;
-	QAngle view_punch_old;
+    QAngle aim_punch_old;
+    QAngle view_punch_old;
 
-	QAngle* aim_punch = nullptr;
-	QAngle* view_punch = nullptr;
+    QAngle* aim_punch = nullptr;
+    QAngle* view_punch = nullptr;
 
-	if (I::Engine->IsInGame()) 
-	{
-		if (stage == FRAME_RENDER_START && Vars.Visuals.Removals.VisualRecoil)
-		{
-			if (G::LocalPlayer && G::LocalPlayer->GetAlive())
-			{
-				aim_punch = (QAngle*)((DWORD)G::LocalPlayer + offsets.m_aimPunchAngle);
-				view_punch = (QAngle*)((DWORD)G::LocalPlayer + offsets.m_viewPunchAngle);
+    if(I::Engine->IsInGame())
+    {
+        if(stage == FRAME_RENDER_START)
+        {
+            if(G::LocalPlayer && G::LocalPlayer->GetAlive()
+                && Vars.Visuals.Removals.VisualRecoil)
+            {
+                aim_punch = 
+                    (QAngle*)((DWORD)G::LocalPlayer + offsets.m_aimPunchAngle);
 
-				aim_punch_old = *aim_punch;
-				view_punch_old = *view_punch;
+                view_punch = 
+                    (QAngle*)((DWORD)G::LocalPlayer + offsets.m_viewPunchAngle);
 
-				*aim_punch = QAngle(0, 0, 0);
-				*view_punch = QAngle(0, 0, 0);
-			}
-		}
-		if (stage == FRAME_RENDER_START)
-		{
-			for (int i = 1; i <= I::Globals->maxClients; i++)
-			{
-				if (i == I::Engine->GetLocalPlayer()) continue;
+                aim_punch_old = *aim_punch;
+                view_punch_old = *view_punch;
 
-				CBaseEntity* pCurEntity = I::ClientEntList->GetClientEntity(i);
-				if (!pCurEntity) continue;
+                *aim_punch = QAngle(0, 0, 0);
+                *view_punch = QAngle(0, 0, 0);
+            }
 
-				*(int*)((uintptr_t)pCurEntity + 0xA30) = I::Globals ->framecount;
-				*(int*)((uintptr_t)pCurEntity + 0xA28) = 0;
-			}
-		}
-	}	
+            for(int i = 1; i <= I::Globals->maxClients; i++)
+            {
+                if(i == I::Engine->GetLocalPlayer())
+                    continue;
 
-	oFrameStageNotify( stage );
+                CBaseEntity* pCurEntity = I::ClientEntList->GetClientEntity(i);
 
-	if( aim_punch && view_punch && Vars.Visuals.Removals.VisualRecoil )
-	{
-		*aim_punch = aim_punch_old;
-		*view_punch = view_punch_old;
-	}
+                if(!pCurEntity)
+                    continue;
+
+                *(int*)((uintptr_t)pCurEntity + 0xA30) = I::Globals->framecount;
+                *(int*)((uintptr_t)pCurEntity + 0xA28) = 0;
+            }
+        }
+    }
+
+    oFrameStageNotify(stage);
+
+    if(aim_punch && view_punch && Vars.Visuals.Removals.VisualRecoil)
+    {
+        *aim_punch = aim_punch_old;
+        *view_punch = view_punch_old;
+    }
 }
