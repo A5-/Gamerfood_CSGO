17952a47ea1663beb99432871a17c00c9e906bf2
steel4me <26707054+steel4me@users.noreply.github.com>
2017-08-19 10:00:54 +0200

    Update Entity.h

--- csgo_internal/csgo_internal/Entity.h
+++ csgo_internal/csgo_internal/Entity.h
@@ -115,134 +115,120 @@ public:
 class WeaponInfo_t
 {
 public:
-	BYTE pad_0x0000[4]; //0x0000
-	bool bParsedScript; //0x0004
-	bool bLoadedHudElements; //0x0005
-	char szClassName[80]; //0x0006
-	char szPrintName[80]; //0x0056
-	char szViewModel[80]; //0x00A6
-	char szWorldModel[80]; //0x00F6
-	char szBulletType[32]; //0x0146
-	char szWorldDroppedModel[80];//0x0166
-	char szAnimationPrefix[16]; //0x01B6
-	BYTE pad_0x01C6[2]; //0x01C6
-	int iSlot; //0x01C8
-	int iPosition; //0x01CC
-	int iMaxClip1; //0x01D0
-	int iMaxClip2; //0x01D4
-	int iDefaultClip1; //0x01D8
-	int iDefaultClip2; //0x01DC
-	int iWeight; //0x01E0
-	int iRumbleEffect; //0x01E4
-	bool bAutoSwitchTo; //0x01E8
-	bool bAutoSwitchFrom; //0x01E9
-	BYTE pad_0x01EA[2]; //0x01EA
-	int iFlags; //0x01EC
-	char szAmmo1[16]; //0x01F0
-	char szAmmo2[16]; //0x0200
-	char szAIAddOn[80]; //0x0210
-	char aShootSounds[17][80]; //0x0260
-	char PAD_NEW[80];
-	int iAmmoType;
-	int iAmmo2Type;
-	bool bMeleeWeapon;
-	bool bBuiltRightHanded;
-	bool bAllowFlipping;
-	BYTE pad_0x07BB;
-	int iSpriteCount;
-	CHudTexture* iconWeaponS;
-	CHudTexture* iconWeapon;
-	CHudTexture* iconAmmo;
-	CHudTexture* iconAmmo2;
-	CHudTexture* iconCrosshair;
-	CHudTexture* iconAutoaim;
-	CHudTexture* iconZoomedCrosshair;
-	CHudTexture* iconZoomedAutoaim;
-	CHudTexture* iconWeaponSmall;
-	bool bShowUsageHint;
-	BYTE pad_0x07E5[3];
-	CSWeaponType iWeaponType;
-	int iTeam;
-	int iWeaponID;
-	bool bFullAuto;
-	BYTE pad_0x07F5[3];
-	float flHeatPerShot;
-	int iWeaponPrice;
-	float flArmorRatio;
-	float flMaxPlayerSpeed;
-	float flMaxPlayerSpeedAlt;
-	int iCrosshairMinDistance;
-	int iCrosshairDeltaDistance;
-	float flPenetration;
-	int iDamage;
-	float flRange;
-	float flRangeModifier;
-	int iBullets;
-	float flCycleTime;
-	float flCycleTimeAlt;
-	char szHeatEffect[80];
-	float aflSmokeColor[3];
-	char szMuzzleFlashEffect_1stPerson[80];
-	char szMuzzleFlashEffect_3rdPerson[80];
-	char szEjectBrassEffect[80];
-	char szTracerEffect[80];
-	int iTracerFrequency;
-	float flSpread;
-	float flSpreadAlt;
-	float flInaccuracyCrouch;
-	float flInaccuracyCrouchAlt;
-	float flInaccuracyStand;
-	float flInaccuracyStandAlt;
-	float flInaccuracyJump;
-	float flInaccuracyJumpAlt;
-	float flInaccuracyLand;
-	float flInaccuracyLandAlt;
-	float flInaccuracyLadder;
-	float flInaccuracyLadderAlt;
-	float flInaccuracyFire;
-	float flInaccuracyFireAlt;
-	float flInaccuracyMove;
-	float flInaccuracyMoveAlt;
-	float flRecoveryTimeStand;
-	float flRecoveryTimeCrouch;
-	float flInaccuracyReload;
-	float flInaccuracyAltSwitch;
-	float flRecoilAngle;
-	float flRecoilAngleAlt;
-	float flRecoilAngleVariance;
-	float flRecoilAngleVarianceAlt;
-	float flRecoilMagnitude;
-	float flRecoilMagnitudeAlt;
-	float flRecoilMagnitudeVariance;
-	float flRecoilMagnitudeVarianceAlt;
-	int iRecoilSeed;
-	float flFlinchVelocityModifierLarge;
-	float flFlinchVelocityModifierSmall;
-	float flTimeToIdle;
-	float flIdleInterval;
-	float recoilTable[2][128];
-	int iZoomLevels;
-	int iZoomFOV[3];
-	float fZoomTime[3];
-	bool bHideViewModelZoomed;
-	char szZoomInSound[80];
-	char szZoomOutSound[80];
-	BYTE pad_0x0F11[3];
-	float flBotAudibleRange;
-	bool bCanUseWithShield;
-	char szWrongTeamMsg[32];
-	char szAnimPrefix[16];
-	char szShieldViewModel[64];
-	char szAddonModel[80];
-	char szAddonLocation[80];
-	char szSilencerModel[80];
-	BYTE pad_0x1079[3];
-	float flAddonScale;
-	float flThrowVelocity;
-	int iKillAward;
-	BYTE pad_0x1088[8];
+	char pad_0x0000[4]; // 0x0000
+	char* consoleName; // 0x0004 
+	char pad_0008[12]; // 0x0008 
+	int iMaxClip1; // 0x0014 
+	int iMaxClip2; // 0x0018 
+	int iDefaultClip1; // 0x001C 
+	int iDefaultClip2; // 0x0020 
+	char pad_0024[8]; // 0x0024 
+	char* szWorldModel; // 0x002C 
+	char* szViewModel; // 0x0030 
+	char* szDroppedModel; // 0x0034 
+	char pad_0038[4]; // 0x0038 
+	char* N0000023E; // 0x003C 
+	char pad_0040[56]; // 0x0040 
+	char* szEmptySound; // 0x0078 
+	char pad_007C[4]; // 0x007C 
+	char* szBulletType; // 0x0080 
+	char pad_0084[4]; // 0x0084 
+	char* szHudName; // 0x0088 
+	char* szWeaponName; // 0x008C 
+	char pad_0090[56]; // 0x0090 
+	int WeaponType; // 0x00C8 
+	int iWeaponPrice; // 0x00CC 
+	int iKillAward; // 0x00D0 
+	char* szAnimationPrefix; // 0x00D4 
+	float flCycleTime; // 0x00D8 
+	float flCycleTimeAlt; // 0x00DC 
+	float flTimeToIdle; // 0x00E0 
+	float flIdleInterval; // 0x00E4 
+	bool bFullAuto; // 0x00E8 
+	char pad_0x00E5[3]; // 0x00E9 
+	int iDamage; // 0x00EC 
+	float flArmorRatio; // 0x00F0 
+	int iBullets; // 0x00F4 
+	float flPenetration; // 0x00F8 
+	float flFlinchVelocityModifierLarge; // 0x00FC 
+	float flFlinchVelocityModifierSmall; // 0x0100 
+	float flRange; // 0x0104 
+	float flRangeModifier; // 0x0108 
+	float flThrowVelocity; // 0x010C 
+	char pad_0x010C[12]; // 0x0110 
+	bool bHasSilencer; // 0x011C 
+	char pad_0x0119[3]; // 0x011D 
+	char* pSilencerModel; // 0x0120 
+	int iCrosshairMinDistance; // 0x0124 
+	int iCrosshairDeltaDistance;// 0x0128 - iTeam? 
+	float flMaxPlayerSpeed; // 0x012C 
+	float flMaxPlayerSpeedAlt; // 0x0130 
+	float flSpread; // 0x0134 
+	float flSpreadAlt; // 0x0138 
+	float flInaccuracyCrouch; // 0x013C 
+	float flInaccuracyCrouchAlt; // 0x0140 
+	float flInaccuracyStand; // 0x0144 
+	float flInaccuracyStandAlt; // 0x0148 
+	float flInaccuracyJumpInitial;// 0x014C 
+	float flInaccuracyJump; // 0x0150 
+	float flInaccuracyJumpAlt; // 0x0154 
+	float flInaccuracyLand; // 0x0158 
+	float flInaccuracyLandAlt; // 0x015C 
+	float flInaccuracyLadder; // 0x0160 
+	float flInaccuracyLadderAlt; // 0x0164 
+	float flInaccuracyFire; // 0x0168 
+	float flInaccuracyFireAlt; // 0x016C 
+	float flInaccuracyMove; // 0x0170 
+	float flInaccuracyMoveAlt; // 0x0174 
+	float flInaccuracyReload; // 0x0178 
+	int iRecoilSeed; // 0x017C 
+	float flRecoilAngle; // 0x0180 
+	float flRecoilAngleAlt; // 0x0184 
+	float flRecoilAngleVariance; // 0x0188 
+	float flRecoilAngleVarianceAlt; // 0x018C 
+	float flRecoilMagnitude; // 0x0190 
+	float flRecoilMagnitudeAlt; // 0x0194 
+	float flRecoilMagnitudeVariance; // 0x0198 
+	float flRecoilMagnitudeVarianceAlt; // 0x019C 
+	float flRecoveryTimeCrouch; // 0x01A0 
+	float flRecoveryTimeStand; // 0x01A4 
+	float flRecoveryTimeCrouchFinal; // 0x01A8 
+	float flRecoveryTimeStandFinal; // 0x01AC 
+	int iRecoveryTransitionStartBullet;// 0x01B0 
+	int iRecoveryTransitionEndBullet; // 0x01B4 
+	bool bUnzoomAfterShot; // 0x01B8 
+	bool bHideViewModelZoomed; // 0x01B9 
+	char pad_0x01B5[2]; // 0x01BA 
+	char iZoomLevels[3]; // 0x01BC 
+	int iZoomFOV[2]; // 0x01C0 
+	float fZoomTime[3]; // 0x01C4 
+	char* szWeaponClass; // 0x01D4 
+	float flAddonScale; // 0x01D8 
+	char pad_0x01DC[4]; // 0x01DC 
+	char* szEjectBrassEffect; // 0x01E0 
+	char* szTracerEffect; // 0x01E4 
+	int iTracerFrequency; // 0x01E8 
+	int iTracerFrequencyAlt; // 0x01EC 
+	char* szMuzzleFlashEffect_1stPerson; // 0x01F0 
+	char pad_0x01F4[4]; // 0x01F4 
+	char* szMuzzleFlashEffect_3rdPerson; // 0x01F8 
+	char pad_0x01FC[4]; // 0x01FC 
+	char* szMuzzleSmokeEffect; // 0x0200 
+	float flHeatPerShot; // 0x0204 
+	char* szZoomInSound; // 0x0208 
+	char* szZoomOutSound; // 0x020C 
+	float flInaccuracyPitchShift; // 0x0210 
+	float flInaccuracySoundThreshold; // 0x0214 
+	float flBotAudibleRange; // 0x0218 
+	BYTE pad_0x0218[8]; // 0x0220 
+	char* pWrongTeamMsg; // 0x0224 
+	bool bHasBurstMode; // 0x0228 
+	BYTE pad_0x0225[3]; // 0x0229 
+	bool bIsRevolver; // 0x022C 
+	bool bCannotShootUnderwater; // 0x0230 };
 };
 
+/* for what please?
 class CCSWeaponInfo_t
 {
 public:
@@ -287,6 +273,7 @@ public:
 	__int32 iBullets; //0x0784
 	float CycleTime; //0x0788
 };//Size=0x078C
+*/
 
 class CBaseCombatWeapon
 {
