c9cfec74acb3bd72b1cf4db98cafd4ee169ad765
A5- <a5@0wn3d.me>
2017-08-24 10:48:50 +0100

    Removed old weapon info struct
    Just for cleanup

--- csgo_internal/csgo_internal/Entity.h
+++ csgo_internal/csgo_internal/Entity.h
@@ -228,53 +228,6 @@ public:
 	bool bCannotShootUnderwater; // 0x0230 };
 };
 
-/* for what please?
-class CCSWeaponInfo_t
-{
-public:
-	void* vTable; //0x0000
-	BYTE N0309F389; //0x0004
-	BYTE N030AC4C1; //0x0005
-	char name_class[ 93 ]; //0x0006
-	char printname[ 80 ]; //0x00
-	char viewmodel[ 80 ]; //0x00A6
-	char playermodel[ 80 ]; //0x00F6
-	char bullettype[ 32 ]; //0x0146
-	char anim_prefix[ 58 ]; //0x0166
-	char N03122066[ 32 ]; //0x01A0
-	char contentpack[ 32 ]; //0x01C0
-	char unknowndata00[ 128 ]; //0x01E0
-	char name_real[ 32 ]; //0x0260
-	char unknowndata01[ 1184 ]; //0x0280
-	CHudTexture* hudTexture_silenced; //0x0720
-	CHudTexture* hudTexture; //0x0724
-	CHudTexture* hudTexture_ammo; //0x0728
-	char unknowndata02[ 4 ]; //0x072C
-	CHudTexture* hudTexture_crosshair_01; //0x0730
-	CHudTexture* hudTexture_crosshair_02; //0x0734
-	CHudTexture* hudTexture_crosshair_03; //0x0738
-	CHudTexture* hudTexture_crosshair_04; //0x073C
-	char unknowndata03[ 8 ]; //0x0740
-	__int32 WeaponType; //0x0748
-	__int32 Team; //0x074C
-	__int32 WeaponID; //0x0750
-	BYTE FullAuto; //0x0754
-	char unknowndata04[ 7 ]; //0x0755
-	__int32 WeaponPrice; //0x075C
-	float WeaponArmorRatio; //0x0760
-	float MaxPlayerSpeed; //0x0764
-	float MaxPlayerSpeedAlt; //0x0768
-	__int32 CrosshairMinDistance; //0x076C
-	__int32 CrosshairDeltaDistance; //0x0770
-	float flPenetrationPower; //0x0774
-	__int32 iDamage; //0x0778
-	float flRange; //0x077C
-	float flRangeModifier; //0x0780
-	__int32 iBullets; //0x0784
-	float CycleTime; //0x0788
-};//Size=0x078C
-*/
-
 class CBaseCombatWeapon
 {
 public:
